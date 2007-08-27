be823326be87ede15d613fcbab3f4d99f0000291
clearskys <clearskys@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-08-27 07:22:34 +0000

    Updated script cleansing
    git-svn-id: https://plugins.svn.wordpress.org/mydashboard/trunk@17443 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mydashboard.php
+++ mydashboard.php
@@ -100,33 +100,33 @@ class CSMyDashboard {
 	function handle_ajax() {
 		// This function receives all ajax calls for the gadgets, and passes them off
 		// to the relevant user functions
-		if($this->onpage('index.php') && $_GET['call'] == '_ajax' && function_exists('current_user_can') && current_user_can('edit_posts')) {
+		if($this->onpage('wp-admin/index.php') && $this->xss_clean($_GET['call']) == '_ajax' && function_exists('current_user_can') && current_user_can('edit_posts')) {
 			
 			$mdp = get_option("clearskys_dashboard_pages");
 			$this->load_stored_gadgets();
 			
 			
-			switch($_GET['action']) {
+			switch($this->xss_clean($_GET['action'])) {
 				case "updatecontent":
 					//echo $this->get_gadget_content($_GET['name']);
-					echo $this->show_gadget_box($_GET['name']);
+					echo $this->show_gadget_box($this->xss_clean($_GET['name']));
 					break;
 				case "updatetitle":
 					//echo $this->get_gadget_title($_GET['name']);
 					break;
 				case "sendedit";
-					$sent = $_POST['update'];
+					$sent = $this->xss_clean($_POST['update']);
 					if($sent != '') {
 						echo $this->show_gadget_box($sent);
 					}
 					break;
 				case "reordercolumn":
-					$tcolumn = $_GET['column'];
+					$tcolumn = $this->xss_clean($_GET['column']);
 					if(isset($mdp['page-1'][$tcolumn]) && count($mdp['page-1'][$tcolumn]) > 0) {
 						// make sure that the column actually exists and it has
 						// gadgets on it then
 						// create an array of gadget names in the order we want them
-						$gadgets = explode('&',$_GET['gadgets']);
+						$gadgets = explode('&',$this->xss_clean($_GET['gadgets']));
 						for($n=0; $n < count($gadgets); $n++) {
 							$gadgets[$n] = str_replace($tcolumn . '[]=', '', $gadgets[$n]);
 						}
@@ -148,13 +148,13 @@ class CSMyDashboard {
 					}
 					break;
 				case "movegadget":
-					$tcolumn = $_GET['tocolumn'];
-					$fcolumn = $_GET['fromcolumn'];
+					$tcolumn = $this->xss_clean($_GET['tocolumn']);
+					$fcolumn = $this->xss_clean($_GET['fromcolumn']);
 					if(isset($mdp['page-1'][$fcolumn]) && isset($mdp['page-1'][$tcolumn]) && (count($mdp['page-1'][$fcolumn]) > 0 || count($mdp['page-1'][$tcolumn]) > 0)) {
 						// make sure that the columns exist and that there is actually something to move from one
 						// column to the other.	
 						// Handle the to column first
-						$gadgets = explode('&',$_GET['togadgets']);
+						$gadgets = explode('&',$this->xss_clean($_GET['togadgets']));
 						for($n=0; $n < count($gadgets); $n++) {
 							$gadgets[$n] = str_replace($tcolumn . '[]=', '', $gadgets[$n]);
 						}
@@ -170,7 +170,7 @@ class CSMyDashboard {
 							$mdp['page-1'][$tcolumn] = $newpagelayout;
 						}
 						// Now handle the from column first
-						$gadgets = explode('&',$_GET['fromgadgets']);
+						$gadgets = explode('&',$this->xss_clean($_GET['fromgadgets']));
 						for($n=0; $n < count($gadgets); $n++) {
 							$gadgets[$n] = str_replace($fcolumn . '[]=', '', $gadgets[$n]);
 						}
@@ -192,7 +192,7 @@ class CSMyDashboard {
 					}
 					break;
 				case "removegadget":
-					$gadget = $_GET['gadget'];
+					$gadget = $this->xss_clean($_GET['gadget']);
 					if($gadget != "" && $this->gadget_active($gadget)) {
 						// Check a gadget is passed and that it is active
 						// remove the gadget from the gadget instances
@@ -220,7 +220,7 @@ class CSMyDashboard {
 				case "addgadget":
 					if(isset($mdp['page-1'])) {
 						// Get the Left hand column
-						$toadd = $_GET['gadget'];
+						$toadd = $this->xss_clean($_GET['gadget']);
 						if($toadd != "") {
 							$toadd = str_replace('addtopage_','',$toadd);
 						}
@@ -315,7 +315,7 @@ class CSMyDashboard {
 		
 		$md = get_option("clearskys_dashboard_config");
 		
-		if(($this->onpage('wp-admin/index.php') || $this->onpage('wp-admin/admin.php')) && $_GET['page'] == $this->mylocation) {
+		if(($this->onpage('wp-admin/index.php') || $this->onpage('wp-admin/admin.php')) && $this->xss_clean($_GET['page']) == $this->mylocation) {
 			
 			if(get_bloginfo('version') >= "2.2") {
 			// jquery native for versions from 2.2 and onwards so enqueue the jquery using standard wordpress calls
@@ -356,7 +356,7 @@ class CSMyDashboard {
 	}
 	
 	function redirect_index() {
-		if($this->onpage('wp-admin/index.php') && $_GET['page'] == '' && $_GET['call'] != '_ajax') {
+		if($this->onpage('wp-admin/index.php') && $this->xss_clean($_GET['page']) == '' && $this->xss_clean($_GET['call']) != '_ajax') {
 			// If the call is for the standard index page and isn't an ajax call
 			Header('Location: index.php?page=' . $this->mylocation);
 		}
@@ -937,11 +937,11 @@ class CSMyDashboard {
 		$site_uri = get_settings('siteurl');
 		
 		//print_r($md);
-		if($_POST['submitted'] == 'yes') {
-			if($_POST['show_original'] != $md['show_original']) {
-				$md['show_original'] = $_POST['show_original'];
+		if($this->xss_clean($_POST['submitted']) == 'yes') {
+			if($this->xss_clean($_POST['show_original']) != $md['show_original']) {
+				$md['show_original'] = $this->xss_clean($_POST['show_original']);
 			}
-			if($_POST['skin'] != $md['skin']) {
+			if($this->xss_clean($_POST['skin']) != $md['skin']) {
 				$md['skin'] = $_POST['skin'];
 			}
 			update_option("clearskys_dashboard_config",$md);
@@ -1007,7 +1007,7 @@ class CSMyDashboard {
 	}
 	
 	function onpage($page) {
-		$path = $_SERVER["SCRIPT_FILENAME"];
+		$path = $this->xss_clean($_SERVER["SCRIPT_FILENAME"]);
 		
 		if((stristr($path,$page) || stristr($path,$page."?")) && (stristr($path,get_option('upload_path')) === False)) {
 			return True;
@@ -1085,6 +1085,220 @@ class CSMyDashboard {
 		
 	}
 	
+	/* Functions to handle script cleaning - provided thanks to Codeigniter */
+	function xss_clean($str, $charset = 'ISO-8859-1')
+		{	
+			/*
+			 * Remove Null Characters
+			 *
+			 * This prevents sandwiching null characters
+			 * between ascii characters, like Java\0script.
+			 *
+			 */
+			$str = preg_replace('/\0+/', '', $str);
+			$str = preg_replace('/(\\\\0)+/', '', $str);
+	
+			/*
+			 * Validate standard character entities
+			 *
+			 * Add a semicolon if missing.  We do this to enable
+			 * the conversion of entities to ASCII later.
+			 *
+			 */
+			$str = preg_replace('#(&\#*\w+)[\x00-\x20]+;#u',"\\1;",$str);
+			
+			/*
+			 * Validate UTF16 two byte encoding (x00)
+			 *
+			 * Just as above, adds a semicolon if missing.
+			 *
+			 */
+			$str = preg_replace('#(&\#x*)([0-9A-F]+);*#iu',"\\1\\2;",$str);
+	
+			/*
+			 * URL Decode
+			 *
+			 * Just in case stuff like this is submitted:
+			 *
+			 * <a href="http://%77%77%77%2E%67%6F%6F%67%6C%65%2E%63%6F%6D">Google</a>
+			 *
+			 * Note: Normally urldecode() would be easier but it removes plus signs
+			 *
+			 */	
+			$str = preg_replace("/%u0([a-z0-9]{3})/i", "&#x\\1;", $str);
+			$str = preg_replace("/%([a-z0-9]{2})/i", "&#x\\1;", $str);		
+					
+			/*
+			 * Convert character entities to ASCII
+			 *
+			 * This permits our tests below to work reliably.
+			 * We only convert entities that are within tags since
+			 * these are the ones that will pose security problems.
+			 *
+			 */
+			
+			if (preg_match_all("/<(.+?)>/si", $str, $matches))
+			{		
+				for ($i = 0; $i < count($matches['0']); $i++)
+				{
+					$str = str_replace($matches['1'][$i],
+										$this->_html_entity_decode($matches['1'][$i], $charset),
+										$str);
+				}
+			}
+		
+			/*
+			 * Convert all tabs to spaces
+			 *
+			 * This prevents strings like this: ja	vascript
+			 * Note: we deal with spaces between characters later.
+			 *
+			 */		
+			$str = preg_replace("#\t+#", " ", $str);
+		
+			/*
+			 * Makes PHP tags safe
+			 *
+			 *  Note: XML tags are inadvertently replaced too:
+			 *
+			 *	<?xml
+			 *
+			 * But it doesn't seem to pose a problem.
+			 *
+			 */		
+			$str = str_replace(array('<?php', '<?PHP', '<?', '?>'),  array('&lt;?php', '&lt;?PHP', '&lt;?', '?&gt;'), $str);
+		
+			/*
+			 * Compact any exploded words
+			 *
+			 * This corrects words like:  j a v a s c r i p t
+			 * These words are compacted back to their correct state.
+			 *
+			 */		
+			$words = array('javascript', 'vbscript', 'script', 'applet', 'alert', 'document', 'write', 'cookie', 'window');
+			foreach ($words as $word)
+			{
+				$temp = '';
+				for ($i = 0; $i < strlen($word); $i++)
+				{
+					$temp .= substr($word, $i, 1)."\s*";
+				}
+				
+				$temp = substr($temp, 0, -3);
+				$str = preg_replace('#'.$temp.'#s', $word, $str);
+				$str = preg_replace('#'.ucfirst($temp).'#s', ucfirst($word), $str);
+			}
+		
+			/*
+			 * Remove disallowed Javascript in links or img tags
+			 */		
+			 $str = preg_replace("#<a.+?href=.*?(alert\(|alert&\#40;|javascript\:|window\.|document\.|\.cookie|<script|<xss).*?\>.*?</a>#si", "", $str);
+			 $str = preg_replace("#<img.+?src=.*?(alert\(|alert&\#40;|javascript\:|window\.|document\.|\.cookie|<script|<xss).*?\>#si", "", $str);
+			 $str = preg_replace("#<(script|xss).*?\>#si", "", $str);
+	
+			/*
+			 * Remove JavaScript Event Handlers
+			 *
+			 * Note: This code is a little blunt.  It removes
+			 * the event handler and anything up to the closing >,
+			 * but it's unlikely to be a problem.
+			 *
+			 */		
+			 $str = preg_replace('#(<[^>]+.*?)(onblur|onchange|onclick|onfocus|onload|onmouseover|onmouseup|onmousedown|onselect|onsubmit|onunload|onkeypress|onkeydown|onkeyup|onresize)[^>]*>#iU',"\\1>",$str);
+		
+			/*
+			 * Sanitize naughty HTML elements
+			 *
+			 * If a tag containing any of the words in the list
+			 * below is found, the tag gets converted to entities.
+			 *
+			 * So this: <blink>
+			 * Becomes: &lt;blink&gt;
+			 *
+			 */		
+			$str = preg_replace('#<(/*\s*)(alert|applet|basefont|base|behavior|bgsound|blink|body|embed|expression|form|frameset|frame|head|html|ilayer|iframe|input|layer|link|meta|object|plaintext|style|script|textarea|title|xml|xss)([^>]*)>#is', "&lt;\\1\\2\\3&gt;", $str);
+			
+			/*
+			 * Sanitize naughty scripting elements
+			 *
+			 * Similar to above, only instead of looking for
+			 * tags it looks for PHP and JavaScript commands
+			 * that are disallowed.  Rather than removing the
+			 * code, it simply converts the parenthesis to entities
+			 * rendering the code un-executable.
+			 *
+			 * For example:	eval('some code')
+			 * Becomes:		eval&#40;'some code'&#41;
+			 *
+			 */
+			$str = preg_replace('#(alert|cmd|passthru|eval|exec|system|fopen|fsockopen|file|file_get_contents|readfile|unlink)(\s*)\((.*?)\)#si', "\\1\\2&#40;\\3&#41;", $str);
+							
+			/*
+			 * Final clean up
+			 *
+			 * This adds a bit of extra precaution in case
+			 * something got through the above filters
+			 *
+			 */	
+			$bad = array(
+							'document.cookie'	=> '',
+							'document.write'	=> '',
+							'window.location'	=> '',
+							"javascript\s*:"	=> '',
+							"Redirect\s+302"	=> '',
+							'<!--'				=> '&lt;!--',
+							'-->'				=> '--&gt;'
+						);
+		
+			foreach ($bad as $key => $val)
+			{
+				$str = preg_replace("#".$key."#i", $val, $str);
+			}
+			
+							
+			return $str;
+		}
+		
+		function _html_entity_decode($str, $charset='ISO-8859-1')
+		{
+			if (stristr($str, '&') === FALSE) return $str;
+		
+			// The reason we are not using html_entity_decode() by itself is because
+			// while it is not technically correct to leave out the semicolon
+			// at the end of an entity most browsers will still interpret the entity
+			// correctly.  html_entity_decode() does not convert entities without
+			// semicolons, so we are left with our own little solution here. Bummer.
+		
+			if (function_exists('html_entity_decode') && (strtolower($charset) != 'utf-8' OR version_compare(phpversion(), '5.0.0', '>=')))
+			{
+				$str = html_entity_decode($str, ENT_COMPAT, $charset);
+				$str = preg_replace('~&#x([0-9a-f]{2,5})~ei', 'chr(hexdec("\\1"))', $str);
+				return preg_replace('~&#([0-9]{2,4})~e', 'chr(\\1)', $str);
+			}
+			
+			// Numeric Entities
+			$str = preg_replace('~&#x([0-9a-f]{2,5});{0,1}~ei', 'chr(hexdec("\\1"))', $str);
+			$str = preg_replace('~&#([0-9]{2,4});{0,1}~e', 'chr(\\1)', $str);
+		
+			// Literal Entities - Slightly slow so we do another check
+			if (stristr($str, '&') === FALSE)
+			{
+				$str = strtr($str, array_flip(get_html_translation_table(HTML_ENTITIES)));
+			}
+			
+			return $str;
+		}
+		
+		function is_malicious($input) {
+		$is_m = false;
+		$bad_inputs = array("\r", "\n", "mime-version", "content-type", "cc:", "to:");
+		foreach($bad_inputs as $bad_input) {
+			if(strpos(strtolower($input), strtolower($bad_input)) !== false) {
+				$is_m = true; break;
+			}
+		}
+		return $is_m;
+	}
 }
 
 function register_mydashboard_gadget($name, $options = array(), $type = 'standard') {
